b59deaa215d79e130563c09cdab17252aeef44c4
PMagombedze <percymagoras@outlook.com>
2024-07-13 01:23:44 +0200

    update app creds

--- app.py
+++ app.py
@@ -122,4 +122,4 @@ with app.app_context():
     db.create_all()
 
 if __name__ == "__main__":
-    app.run(port=5000, debug=False)
+    app.run(port=5000, debug=True)
--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config:
 
     dotenv.load_dotenv()
 
-    DEBUG = False
+    DEBUG = True
     SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_URI")
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
     SECRET_KEY = os.getenv("SECRET_KEY")
